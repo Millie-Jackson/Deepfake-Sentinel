5d67c7f7559311d526ca666031edfbf05d2d1899
Millie Jackson <100158073+Millie-Jackson@users.noreply.github.com>
2025-06-19 11:40:54 +0100

    Update app.py

--- src/video_api/app.py
+++ src/video_api/app.py
@@ -4,9 +4,8 @@ from .model import load_model, predict
 app = FastAPI()
 model = load_model()
 
+
 @app.post("/predict")
-
-
 def predict_video(data: dict):
     # TODO: extract frame(s), call predict(model, ...)
     return {"prediction": predict(model, data)}
